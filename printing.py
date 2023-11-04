6313b45f78e324774e556b8c21def11047b42370
ArwinRam22 <ramesanarwin@gmail.com>
2023-11-04 10:15:51 -0400

    Version 1.2
    Pylint update

--- printing.py
+++ printing.py
@@ -1,33 +1,38 @@
-#-------------------------------------------------------
-#printing.py
-#Functions to output vitals tables
-#-------------------------------------------------------
-#Author:            Arwin Ramesan
-#Email:             ramesanarwin@gmail.com
-#Date Created:      2023-03-21
-#Last Modified:     2023-07-15
-#-------------------------------------------------------
-
+"""
+-------------------------------------------------------
+printing.py
+Functions to output vitals tables
+-------------------------------------------------------
+Author:            Arwin Ramesan
+Email:             ramesanarwin@gmail.com
+Date Created:      2023-03-21
+Last Modified:     2023-11-04
+-------------------------------------------------------
+"""
 #Imports
-import reformat
 import re
-#####################################################################################################################
-# Sends each table to the auxiliary method
-    # Parameters
-        #   (String)    starting_pokemon    = the name of the Pokemon
-        #   (String[])  vitals              = list of tables that hold information of pokemon
-        #   (String[])  forms               = list of different forms of the pokemon
+import reformat
+
 def print_basic_info(starting_pokemon, vitals, forms):
+    """
+    Sends each table to the auxiliary method
+    Parameters
+        (String) starting_pokemon = the name of the Pokemon
+        (String[]) vitals = list of tables that hold information of pokemon
+        (String[]) forms = list of different forms of the pokemon
+    """
     form_number = 1 # Starting index for 'forms'
     iterations = 0  # Number of iterations
     num_tables = 4  # Number of tables
     # Data from the following headers require formatting before printing
-        # formatting_required = ['Type', 'Abilities', 'Local №']  
-    
+        # formatting_required = ['Type', 'Abilities', 'Local №']
+
     for vital in vitals:
-        if (iterations % num_tables == 0 or iterations == 0): # Mod by 4 since only displaying 4 tables
-            if (len(forms)-2 == 1): #if Pokemon has no other forms (eg. Bulbasaur, Charmander, etc.)
+        if (iterations % num_tables == 0) or (iterations == 0):
+            #if Pokemon has no other forms (eg. Bulbasaur, Charmander, etc.)
+            if len(forms)-2 == 1:
                 print('<'*25 + f' {starting_pokemon.upper()} ' + '>'*25)
+             #if Pokemon has other forms (eg. Zacian, Meowth, etc.)
             else:
                 print('<'*25 + f' {forms[form_number].upper()} ' + '>'*25)
                 form_number += 1
@@ -41,70 +46,77 @@ def print_basic_info(starting_pokemon, vitals, forms):
             data = row.find('td').text
             data = reformat.replace(data, '\n', "")
 
-            if (header == 'Local №'):
+            if header == 'Local №':
                 locale_data = reformat.reformat_local_no(data)
                 print(f"{'Local №':>15}:")
                 for x in locale_data:
                     print(f"{'':>16} {x}")
             else:
-                if (header == 'Type'):          data = reformat.reformat_type(data)
-                elif (header == 'Abilities'):   data = reformat.reformat_ability(data.replace(' (hidden ability)', '*'))
+                if header == 'Type':
+                    data = reformat.reformat_type(data)
+                elif header == 'Abilities':
+                    data = reformat.reformat_ability(data.replace(' (hidden ability)', '*'))
                 print(f"{header:>15}: {data}")
 
-        if (len(forms)-2 == 1):
+        if len(forms)-2 == 1:
             print('='*(50 + len(starting_pokemon)+2))
         else:
             print('='*(50 + len(forms[form_number])+2))
         iterations += 1
-    
-#####################################################################################################################
-#Prints the Pokemon's moves
-# Parameters
-#   (String)    pokemon_name        = the name of the pokemon
-#   (String[])  panels              = list of tables that hold information of pokemon and other forms
-#   (String[])  forms               = list of different forms of the pokemon
-#   (String[])  regions             = list of different regions in the generation
+
 def print_move_table(pokemon_name, generation, panels, regions):
+    """
+    Prints the Pokemon's moves
+    Parameters
+        (String) pokemon_name = the name of the pokemon
+        (String[]) panels = list of tables that hold information of pokemon and other forms
+        (String[]) forms = list of different forms of the pokemon
+        (String[]) regions = list of different regions in the generation
+    """
+
     print('<'*25 + f' {pokemon_name.upper()} - {generation}' + '>'*25)
 
-    region_count = 1   
+    region_count = 1
     panel_count = 0
     for x in panels:
         #Each region has its own panel
-        if (region_count < len(regions)-1):
+        if region_count < len(regions)-1:
             current_region = regions[region_count]
             current_panel = panels[panel_count]
             excess_panels = print_move_table_aux(current_panel, current_region)
             panel_count += excess_panels
             region_count += 1
 
-#Prints the Pokemon's moves
-# Parameters
-#   (String)    current_panel   = section of html holding numerous move tables
-#   (String)    current_region  = name of region for current tables
 def print_move_table_aux(current_panel, current_region):
+    """
+    Prints the Pokemon's moves
+    Parameters
+        (String) current_panel = section of html holding numerous move tables
+        (String) current_region = name of region for current tables
+    """
+
     print('<'*25 + f'{current_region}' + '>'*25)
-            
+
     #All tables in panel
     all_tables = current_panel.find_all('table', class_ = 'data-table')
     #All tables with tabs
     tables_w_tabs = current_panel.find_all('div', class_ = 'tabset-moves-game-form sv-tabs-wrapper')
     #Headers for the tables
     headers = current_panel.find_all('h3')
-    #Description for the tables            
+    #Description for the tables
     paragraphs = current_panel.find_all('p')
 
     header_count = 0
     table_count = 0
     table_w_tabs_count = 0
     for header in headers:
-        if (len(all_tables) > 0):
+        if len(all_tables) > 0:
             print(headers[header_count].text)
             print(f'\t{paragraphs[header_count].text}')
-            if ('the' in paragraphs[header_count].text):
+            if 'the' in paragraphs[header_count].text:
                 print('-'*50)
                 #Current section has 1+ tabs, so multiple tables under same header
-                if (len(tables_w_tabs) > 0 and len(tables_w_tabs) > table_w_tabs_count and tables_w_tabs[table_w_tabs_count].find('table', class_ = 'data-table').text in all_tables[table_count].text):
+                if len(tables_w_tabs) > 0 and len(tables_w_tabs) > table_w_tabs_count and tables_w_tabs[table_w_tabs_count].find('table', class_ = 'data-table').text in all_tables[table_count].text:
 
                     #Determines other forms for current section
                     forms = tables_w_tabs[table_w_tabs_count].find('div', class_ = 'sv-tabs-tab-list').text
@@ -133,7 +145,7 @@ def print_move_table_aux(current_panel, current_region):
                     table_count += 1
             print('='*50)
         header_count += 1
-    
+
     excess_panels = 0
     if len(tables_w_tabs) > 0:
         for t_w_t in tables_w_tabs:
@@ -145,14 +157,16 @@ def print_move_table_aux(current_panel, current_region):
 
     return excess_panels
 
-#Prints each move in a table
-# Parameters
-#   (String[])    moves        = table of moves
-#   (String)  header              = header of table
 def print_move(moves, header):
+    """
+    Prints each move in a table
+    Parameters
+        (String[]) moves = table of moves
+        (String) header = header of table
+    """
     for move in moves:
         numbers = move.find_all('td', class_ = 'cell-num')
-        if (header != 'Egg moves' and header != 'Move Tutor moves' and header != 'Transfer-only moves' and header != 'Pre-evolution moves'):
+        if header not in ('Egg moves', 'Move Tutor moves', 'Transfer-only moves', 'Pre-evolution moves'):
             level_learned = numbers[0].text
             power = numbers[1].text
             accuracy = numbers[2].text
@@ -162,28 +176,29 @@ def print_move(moves, header):
             accuracy = numbers[1].text
 
         name = move.find('td', class_ = 'cell-name').text
-        type = move.find('td', class_ = 'cell-icon').text
+        move_type = move.find('td', class_ = 'cell-icon').text
         category = re.findall('[A-Z][a-z]*', str(move.find('td', class_ = 'cell-icon text-center')))
-        
+
         print(f"{'Move Name':>15}: {name}")
-        print(f"{'Type':>15}: {type}")
+        print(f"{'Type':>15}: {move_type}")
         print(f"{'Category':>15}: {category[0]}")
         print(f"{'Power':>15}: {power}")
         print(f"{'Accuracy':>15}: {accuracy}")
-        if (header == 'Transfer-only moves'):
+        if header == 'Transfer-only moves':
             method = move.find('td', class_ = 'text-small').text
             print(f"{'Method':>15}: {method}")
         print()
 
-#####################################################################################################################
-#Prints table containing list of all moves
-# Parameters
-#   (String)    table        = table containing all moves
 def print_all_moves_table(table):
+    """
+    Prints table containing list of all moves
+    Parameters
+        (String) table = table containing all moves
+    """
     rows = table.find_all('tr')
-    for move in rows:    
+    for move in rows:
         name = move.find('td', class_ = 'cell-name').text
-        type = move.find('td', class_ = 'cell-icon').text
+        move_type = move.find('td', class_ = 'cell-icon').text
         category = re.findall('[A-Z][a-z]*', str(move.find('td', class_ = 'cell-icon text-center')))
         numbers = move.find_all('td', class_ = 'cell-num')
         power = numbers[0].text
@@ -191,23 +206,24 @@ def print_all_moves_table(table):
         power_points = numbers[2].text
         effect = move.find('td', class_ = 'cell-long-text').text
 
-        print(f"{'Move Name':>15}: {name}")        
-        print(f"{'Type':>15}: {type}")
-        if (len(category) > 0):
+        print(f"{'Move Name':>15}: {name}")
+        print(f"{'Type':>15}: {move_type}")
+        if len(category) > 0:
             print(f"{'Category':>15}: {category[0]}")
         else:
             category = '-'
-            print(f"{'Category':>15}: {category}")        
+            print(f"{'Category':>15}: {category}")
         print(f"{'Power':>15}: {power}")
         print(f"{'Accuracy':>15}: {accuracy}")
-        print(f"{'PP':>15}: {power_points}")        
+        print(f"{'PP':>15}: {power_points}")
         print(f"{'Effect':>15}: {effect}\n")
 
-#####################################################################################################################
-#Prints table containing list of all abilities
-# Parameters
-#   (String)    table        = table containing all abilities
 def print_all_abilities(table):
+    """
+    Prints table containing list of all abilities
+    Parameters
+        (String) table = table containing all abilities
+    """
     body = table.find('tbody')
     rows = body.find_all('tr')
     for ability in rows:
@@ -215,7 +231,7 @@ def print_all_abilities(table):
         count = ability.find(class_ = 'cell-num cell-total').text
         desc = ability.find(class_ = 'cell-med-text').text
         generation = ability.find_all(class_ = 'cell-num')
-        
+
         print(f"{'Name':>15}: {name}")
         print(f"{'Description':>15}: {desc}")
         print(f"{'Count':>15}: {count}")
